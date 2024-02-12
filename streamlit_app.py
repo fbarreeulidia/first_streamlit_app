beba79cd95efd4ef775b6ccff6e4a1e3f36f3f0a
fbarreeulidia <156921863+fbarreeulidia@users.noreply.github.com>
2024-02-12 11:25:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
-st.text("Hellvezfeo from Snowflake:")
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
