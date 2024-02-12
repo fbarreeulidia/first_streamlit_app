86a6446a7f8233d6aa9c6fd8047b1618ccf51502
fbarreeulidia <156921863+fbarreeulidia@users.noreply.github.com>
2024-02-12 09:25:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,9 @@ st.dataframe(fruits_to_show)
 
 
 st.header("Fruityvice Fruit Advice!")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
